0ea56f34a7119707fdbf1772cff9c584ce45946f
JallaJenkins <68257894+JallaJenkins@users.noreply.github.com>
2022-12-10 18:40:12 -0800

    Dec 2022 update

--- script.js
+++ script.js
@@ -25,9 +25,51 @@ const upcomingEvents = [
     location: `ONLINE`,
     link: "https://wise_intention.eventbrite.ca",
   },
-];
-
-const eventsInDevelopment = [
+  {
+    startDate: new Date(2022, 11, 26),
+    showDate: new Date(2022, 11, 31),
+    title: `Days of Mindfulness: a year-end non-residential retreat`,
+    displayDate: `Dec 26-31<br>2022<br>9 AM - 4 PM<br>PST`,
+    summary: `Day-Long Retreat Series`,
+    location: `Mountain Rain Zendo<br>2016 Wall St., Vancouver BC`,
+    link: "https://www.eventbrite.ca/e/days-of-mindfulness-tickets-452376348837",
+  },
+  {
+    startDate: new Date(2023, 0, 14),
+    showDate: new Date(2023, 0, 14),
+    title: `BCIMS community gathering`,
+    displayDate: `14 Jan 2023<br>2-4 PM PST`,
+    summary: `New Year celebration`,
+    location: `2950 Laurel St., Vancouver`,
+    link: "https://www.eventbrite.ca/e/bcims-community-gathering-tickets-472380341317",
+  },
+  {
+    startDate: new Date(2023, 1, 5),
+    showDate: new Date(2023, 1, 5),
+    title: `Meditation Mini-Retreat`,
+    displayDate: `5 Feb 2023<br> 9 AM - 12 PM PST`,
+    summary: `Extended Morning Sit`,
+    location: `2950 Laurel St., Vancouver`,
+    link: "https://www.eventbrite.ca/e/meditation-mini-retreats-tickets-466641175317",
+  },
+  {
+    startDate: new Date(2023, 2, 12),
+    showDate: new Date(2023, 2, 12),
+    title: `Meditation Mini-Retreat`,
+    displayDate: `12 Mar 2023<br> 9 AM - 12 PM PST`,
+    summary: `Extended Morning Sit`,
+    location: `2950 Laurel St., Vancouver`,
+    link: "https://www.eventbrite.ca/e/meditation-mini-retreats-tickets-466641175317",
+  },
+  {
+    startDate: new Date(2023, 3, 16),
+    showDate: new Date(2023, 3, 16),
+    title: `Meditation Mini-Retreat`,
+    displayDate: `16 Apr 2023<br> 9 AM - 12 PM PST`,
+    summary: `Extended Morning Sit`,
+    location: `2950 Laurel St., Vancouver`,
+    link: "https://www.eventbrite.ca/e/meditation-mini-retreats-tickets-466641175317",
+  },
   {
     startDate: new Date(2023, 4, 30),
     showDate: new Date(2023, 4, 30),
@@ -35,6 +77,26 @@ const eventsInDevelopment = [
     displayDate: `May 30 - <br>June 3<br>2023`,
     summary: `Residential Retreat`,
     location: `Bethlehem Retreat Centre in Nanaimo, BC`,
+  },
+];
+
+const eventsInDevelopment = [
+  {
+    startDate: new Date(2023, 0, 2),
+    showDate: new Date(2023, 0, 2),
+    title: `30-day meditation challenge`,
+    displayDate: `Jan 2-31<br>2023`,
+    summary: `Daily guided meditation`,
+    location: `ONLINE (sponsored by IMS)`,
+  },
+  {
+    startDate: new Date(2023, 6, 23),
+    showDate: new Date(2023, 6, 23),
+    title: `Mini-retreat: introduction to meditation`,
+    displayDate: `July 23<br>6-9 AM PT<br>2023`,
+    summary: `Extended morning sit`,
+    location: `ONLINE (sponsored by IMS)`,
+    link: "https://www.eventbrite.ca/e/residential-retreat-with-rachel-lewis-and-dawn-scott-2023may30beth-registration-469412283777",
   },
 ];
 
