88078c9f451f452c1563d33709ae79fcac0b373e
JallaJenkins <wakeupalready@gmail.com>
2024-11-03 14:03:54 -0800

    Update 3 Nov  2024

--- script.js
+++ script.js
@@ -17,24 +17,6 @@ const upcomingEvents = [
     link: "https://www.bcims.org/programs",
   },
   {
-    startDate: new Date(2024, 8, 10),
-    showDate: new Date(2024, 8, 10),
-    title: `IMS Metta Sangha`,
-    displayDate: `Sept 10<br>4-5 PM<br>PT`,
-    summary: `Metta Meditation and Dharma Talk`,
-    location: `ONLINE`,
-    link: "https://ims.dharma.org/courses/metta-sangha-2024",
-  },
-  {
-    startDate: new Date(2024, 9, 10),
-    showDate: new Date(2024, 9, 10),
-    title: `IMS Metta Sangha`,
-    displayDate: `Oct 22<br>4-5 PM<br>PT`,
-    summary: `Metta Meditation and Dharma Talk`,
-    location: `ONLINE`,
-    link: "https://ims.dharma.org/courses/metta-sangha-2024",
-  },
-  {
     startDate: new Date(2024, 10, 4),
     showDate: new Date(2024, 10, 4),
     title: `IMS Metta Sangha`,
@@ -42,42 +24,6 @@ const upcomingEvents = [
     summary: `Metta Meditation and Dharma Talk`,
     location: `ONLINE`,
     link: "https://ims.dharma.org/courses/metta-sangha-2024",
-  },
-  {
-    startDate: new Date(2024, 8, 5),
-    showDate: new Date(2024, 8, 5),
-    title: `In the Buddha's Words: Entering into Emptiness`,
-    displayDate: `Sept 5 & 12<br>4-5:30 PM<br>PT`,
-    summary: `Sutta Study Series`,
-    location: `ONLINE`,
-    link: "https://ims.dharma.org/courses/n-the-buddha-s-words-entering-into-emptiness",
-  },
-  {
-    startDate: new Date(2024, 9, 19),
-    showDate: new Date(2024, 9, 19),
-    title: `Seven Factors of Awakening 8-month online study program`,
-    displayDate: `Oct 2024 -<br>May 2025`,
-    summary: `Intensive study course`,
-    location: `ONLINE`,
-    link: "https://docs.google.com/document/d/1PwJVoR98vai6CQYomtGH0QIR0FTNWhkhYErPROcLz-0/edit?usp=drive_link",
-  },
-  {
-    startDate: new Date(2024, 8, 8),
-    showDate: new Date(2024, 8, 8),
-    title: `Meditation Mini-Retreat`,
-    displayDate: `Sept 8</br>9 AM-12 PM PT`,
-    summary: `Half-day retreat`,
-    location: `ONLINE`,
-    link: "https://www.eventbrite.ca/e/meditation-mini-retreat-tickets-1000992885667?aff=oddtdtcreator",
-  },
-  {
-    startDate: new Date(2024, 9, 27),
-    showDate: new Date(2024, 9, 27),
-    title: `Meditation Mini-Retreat`,
-    displayDate: `Oct 27</br>9 AM-12 PM PT`,
-    summary: `Half-day retreat`,
-    location: `ONLINE`,
-    link: "https://www.eventbrite.ca/e/meditation-mini-retreat-tickets-1000992885667?aff=oddtdtcreator",
   },
   {
     startDate: new Date(2024, 10, 17),
@@ -98,15 +44,6 @@ const upcomingEvents = [
     link: "https://www.eventbrite.ca/e/meditation-mini-retreat-tickets-1000992885667?aff=oddtdtcreator",
   },
   {
-    startDate: new Date(2024, 9, 26),
-    showDate: new Date(2024, 9, 26),
-    title: `Day of Mindfulness with Rachel Lewis`,
-    displayDate: `Oct 26<br>9 AM-4 PM<br>PT`,
-    summary: `Day-long retreat`,
-    location: `2950 Laurel Street, Vancouver (St Mary Magdalene Anglican Church)`,
-    link: "https://www.eventbrite.ca/e/day-of-mindfulness-tickets-1000981120477",
-  },
-  {
     startDate: new Date(2024, 10, 30),
     showDate: new Date(2024, 10, 30),
     title: `Day of Mindfulness with Rachel Lewis`,
@@ -124,9 +61,36 @@ const upcomingEvents = [
     location: `2950 Laurel Street, Vancouver (St Mary Magdalene Anglican Church)`,
     link: "https://www.eventbrite.ca/e/day-of-mindfulness-tickets-1000981120477",
   },
+  {
+    startDate: new Date(2024, 10, 14),
+    showDate: new Date(2024, 10, 21),
+    title: `In the Buddha's Words: the Metta Sutta`,
+    displayDate: `Nov 14 & 21<br>4-5:30 PM<br>PT`,
+    summary: `Online course`,
+    location: `ONLINE`,
+    link: "https://ims.dharma.org/courses/loving-kindness-compassion",
+  },
+  {
+    startDate: new Date(2025, 0, 8),
+    showDate: new Date(2025, 0, 8),
+    title: `The Beautiful Qualities of the Heart`,
+    displayDate: `Jan 8<br>7-8:30 PM<br>PT`,
+    summary: `Online Course`,
+    location: `ONLINE`,
+    link: "https://sati.org/series/brahmavihara-the-beautiful-qualities-of-the-heart/",
+  },
 ];
 
-const eventsInDevelopment = [];
+const eventsInDevelopment = [
+  {
+    startDate: new Date(2025, 0, 2),
+    showDate: new Date(2025, 0, 2),
+    title: `IMS Online: 30 day meditation challenge`,
+    displayDate: `Jan 2 to 31<br>9-9:30 AM<br>PT`,
+    summary: `Online Course`,
+    location: `ONLINE`,
+  },
+];
 
 ///////////////////////////////////////////////////
 // HTML element selectors
